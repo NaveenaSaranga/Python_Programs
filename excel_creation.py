7b7070038a5f3d4838adf52c2f0177e46673d4c9
NaveenaSaranga <46148333+NaveenaSaranga@users.noreply.github.com>
2018-12-28 21:15:02 +0530

    Update excel_creation.py

--- excel_creation.py
+++ excel_creation.py
@@ -6,29 +6,28 @@ from datetime import datetime
 wb = Workbook()
 ws1 = wb.active
 ws1 = wb.create_sheet("sheet1", 0)  # insert at first position
-ws1.title = 'Validation_status'
-for dim in ws1.column_dimensions.values():
-    dim.bestFit = True
-ws1['A1'] = "Assigned POC"
-ws1['B1'] = "DBs"
-ws1['C1'] = "Appdb Validation"
-ws1['D1'] = "Comments"
+#ws1.title = 'sheet1'
 
-print("Enter db names separated by comas:")
-db_list=input().split(',')
+ws1['A1'] = "col1"
+ws1['B1'] = "col2"
+ws1['C1'] = "col3"
+ws1['D1'] = "col4"
 
-print("Enter Assignee names separated by comas:")
-name_list=input().split(',')
+print("Enter values to col1 separated by comas:")
+col1_list=input().split(',')
 
-divNo=len(db_list)/len(name_list)
+print("Enter values to col2 separated by comas:")
+col2_list=input().split(',')
+
+divNo=len(col1_list)/len(col2_list)
 print(divNo)
 final=round(divNo)
-print(final*name_list)
+print(final*col2_list)
 
-new_names=name_list*final
-print(new_names)
-for row, i in enumerate(db_list):
-    for row1, j in enumerate(new_names):
+new_list=col2_list*final
+print(new_list)
+for row, i in enumerate(col1):
+    for row1, j in enumerate(new_list):
         column_cell = 'B'
         ws1[column_cell + str(row + 2)] = str(i)
         print(row1, row)
@@ -38,6 +37,6 @@ for row, i in enumerate(db_list):
         ws1[column_cell+str(row1+2)] = str(j)
 
 
-os.chdir(r'C:\\Users\\nsaranga\\Documents\\AppDb')
+#os.chdir(r'')
 datestring = datetime.strftime(datetime.now(), '%Y_%m_%d')
-wb.save('Refresh_Validation' + datestring + '.xlsx')
+wb.save('file1' + datestring + '.xlsx')
